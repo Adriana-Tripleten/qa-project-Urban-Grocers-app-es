f115c45079e262edf997cf9b1d7a6ea61eb460a5
Adriana Yicela Andrade Trujillo <adrianayicela@hotmail.com>
2025-05-22 10:48:39 -0500

    Descripción del cambio

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-c0260b65-949f-44ce-8ed3-77eca2a6ccf0.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-cb115cd7-9814-46b2-a99e-e203a4516817.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
